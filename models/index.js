6fb5829ce8ddda60f6e48db3e65518745a57daa4
Jamie Williams <jamwil1226@yahoo.com>
2021-11-17 15:38:47 -0500

    request and response routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'id'
+});
 
 module.exports = {
   Product,
